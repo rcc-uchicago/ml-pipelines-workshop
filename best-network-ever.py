4967129f625c3b9c063f871c7095caf9d72c9ac6
J.D. Laurence-Chasen <53494838+jdlaurence@users.noreply.github.com>
2023-02-22 09:57:16 -0600

    update example

--- best-network-ever.py
+++ best-network-ever.py
@@ -1,42 +1,106 @@
+
+# ### A quick neural network training example - time series classification
+# 
+# Adapted from: https://keras.io/examples/timeseries/timeseries_classification_from_scratch/
+
 # import packages
-import pandas as pd
-import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
-from keras.models import Sequential
-from keras.layers import Dense
-from sklearn.model_selection import train_test_split
-
-# Check for GPU engagement
+# Let's do our GPU engagement check again
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 gpus = tf.config.experimental.list_physical_devices('GPU')
-print(tf.config.experimental.get_device_details(gpus[0]))
+try:
+    print(tf.config.experimental.get_device_details(gpus[0]))
+except:
+    print('no dice')
 
-# Download some data about mushrooms
-# #If on a login-node (with internet):
-# url = 'https://raw.githubusercontent.com/rcc-uchicago/ml-pipelines-workshop/main/mushrooms.csv'
-url = '/project/rcc/jdlaurence/ml-pipelines-workshop/mushrooms.csv'
-data = pd.read_csv(url)
+# Now we'll load in the data. The dataset contains 3601 training instances and another 1320 testing instances. Each timeseries corresponds to a measurement of engine noise captured by a motor sensor. For this task, the goal is to automatically detect the presence of a specific issue with the engine.
+# Define a simple import function
+def readucr(filename):
+    data = np.loadtxt(filename, delimiter="\t")
+    y = data[:, 0]
+    x = data[:, 1:]
+    return x, y.astype(int)
 
-# Prepare data for network
-data2 = pd.get_dummies(data) # One-hot encoding of categorical/string variables
+# # If on a login node (connected to internet)
+# data_path = "https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/"
 
-# Train-test-split
-X = data2.drop(['class_e', 'class_p'], axis=1)
-y = data2[['class_e', 'class_p']]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, shuffle=True)
+# # Else:
+data_path = '/project/rcc/jdlaurence/ml-pipelines-workshop/FordA/'
 
-# define model
-model = Sequential()
-model.add(Dense(units=16, activation='sigmoid', input_shape=[X_train.shape[1]])) # 16 neuron layer
-model.add(Dense(2, activation='softmax')) # Binary output
+# Read data, already split into test/train
+x_train, y_train = readucr(data_path + "FordA_TRAIN.tsv")
+x_test, y_test = readucr(data_path + "FordA_TEST.tsv")
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics='accuracy')
+# Next, we'll do some quick data formatting/prep
 
-# Train model
-model.fit(X_train, y_train, epochs=25, verbose=0)
+# Reformat data for network
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
 
-# Evaluate
-loss_and_acc = model.evaluate(X_test,y_test)
-print('Loss: ',loss_and_acc[0])
-print('Accuracy: ', loss_and_acc[1])
+# Shuffle training data
+num_classes = len(np.unique(y_train))
+idx = np.random.permutation(len(x_train))
+x_train = x_train[idx]
+y_train = y_train[idx]
+
+# Set class -1 to be 0
+y_train[y_train == -1] = 0
+y_test[y_test == -1] = 0
+
+# Define a convolutional neural network with three 1-D convolutional layers 
+def make_model(input_shape):
+    input_layer = keras.layers.Input(input_shape)
+
+    conv1 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(input_layer)
+    conv1 = keras.layers.BatchNormalization()(conv1)
+    conv1 = keras.layers.ReLU()(conv1)
+
+    conv2 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv1)
+    conv2 = keras.layers.BatchNormalization()(conv2)
+    conv2 = keras.layers.ReLU()(conv2)
+
+    conv3 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv2)
+    conv3 = keras.layers.BatchNormalization()(conv3)
+    conv3 = keras.layers.ReLU()(conv3)
+
+    gap = keras.layers.GlobalAveragePooling1D()(conv3)
+
+    output_layer = keras.layers.Dense(num_classes, activation="softmax")(gap)
+
+    return keras.models.Model(inputs=input_layer, outputs=output_layer)
+
+
+model = make_model(input_shape=x_train.shape[1:])
+
+# Compile the model and train it for 50 epochs
+epochs = 50 # Should be closer to 200 for performance plateau
+batch_size = 32
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["sparse_categorical_accuracy"],
+)
+history = model.fit(
+    x_train,
+    y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    validation_split=0.2,
+    verbose=1,
+)
+
+# %% [markdown]
+# Evaluate the network
+
+# %%
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print("Test accuracy", test_acc)
+print("Test loss", test_loss)
+
+
